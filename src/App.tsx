be8fc07aacbcbb066fc56f775cb8861647b6a49d
vincent <gaobing1230@gmail.com>
2023-01-06 17:03:45 +0800

    add words

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Phonetic } from "./components/Phonetic";
 import { RandomNum } from "./components/RandomNum";
 import { WordGroup } from "./components/WordGroup";
 import nationalite from "./data/nationalite.json";
+import professions from "./data/professions.json";
 import semaine from "./data/semaine.json";
 import unit1 from "./data/unit1.json";
 import unit2 from "./data/unit2.json";
@@ -16,6 +17,7 @@ function App() {
       <Route path="/num" element={<RandomNum />} />
       <Route path="nationalité" element={<WordGroup words={nationalite} />} />
       <Route path="semaine" element={<WordGroup words={semaine} />} />
+      <Route path="professions" element={<WordGroup words={professions} />} />
       <Route path="unité-1" element={<WordGroup words={unit1} />} />
       <Route path="unité-2" element={<WordGroup words={unit2} />} />
     </Routes>
